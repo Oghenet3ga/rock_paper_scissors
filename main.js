7254445b648bef1c8c8de08d7f320749993ee69c
Oghenet3ga <oghenetega.f@yahoo.com>
2023-08-23 16:20:14 +0100

    Rock, paper, scissors game js

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 function getComputerChoice(){
-let choices=['Rock', 'Paper', 'Scissors'];
-return choices[Math.floor(Math.random()* choices.length)];
-}
-const computerSelection=getComputerChoice()
+    let choices=['Rock', 'Paper', 'Scissors'];
+    return choices[Math.floor(Math.random()* choices.length)];
+    }
+    
 
 function startGame(){
-    let playerSelection=prompt("Rock, Paper or Scissors?").toLowerCase();
-    alert(`Computer chose: ${computerSelection}` )
-    if(playerSelection==="rock" && computerSelection==="Paper"){
-        alert("You Lose! Paper beats Rock")
+    
+const playerSelection=prompt("Pick your poison: Rock, Paper or Scissors?").toLowerCase();
+const computerSelection=getComputerChoice();
+
+    if (playerSelection===computerSelection.toLowerCase()){
+        return "That's a tie!"
     }
+    else if ((playerSelection==="rock" && computerSelection==="Paper") ||( playerSelection==="paper" && computerSelection==="Scissors") || (playerSelection==="scissors" && computerSelection==="Rock")){
+            return "You Lose!"
+        }
+    else {
+            return "You Win!"
 }
-startGame()
+}
+
+function game(){
+    let playerWins=0;
+    let computerChoice;
+    for(let round=0; round < 5; round++){
+      const result=startGame();
+      computerChoice = getComputerChoice();
+        if (result==="You Win!"){
+            playerWins++
+        }
+alert(`Computer chose: ${computerChoice}. ${result}`);
+    }
+    if (playerWins >= 3) {
+        return "You won this round. Congratulations!"
+    }
+else {return "You lost this round. Try again"}
+}
+const finalresult=game();
+alert(finalresult);
